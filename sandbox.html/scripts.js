7a7116ece08066c65bc082c7b5a766bb13f7f21c
22DP1MZied <131352681+22DP1MZied@users.noreply.github.com>
2024-09-27 16:02:08 +0300

    Update scripts.js

--- sandbox.html/scripts.js
+++ sandbox.html/scripts.js
@@ -1,22 +1,20 @@
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
 }
 
 function openModal() {
-    const modal = document.getElementById("myModal");
-    modal.style.display = "block";
+    document.getElementById("myModal").style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("myModal");
-    modal.style.display = "none";
+    document.getElementById("myModal").style.display = "none";
 }
 
-// Close the modal when the user clicks anywhere outside of it
+// Aizvērt modālo logu, ja noklikšķina ārpus tā
 window.onclick = function(event) {
     const modal = document.getElementById("myModal");
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
