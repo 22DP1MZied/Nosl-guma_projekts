5ffcbf6025ff0863e76902ba40f94c5cfdb6a73b
22DP1MZied <131352681+22DP1MZied@users.noreply.github.com>
2024-10-11 15:25:53 +0300

    Update scripts.js

--- sandbox.html/scripts.js
+++ sandbox.html/scripts.js
@@ -1,22 +1,3 @@
-/*// Function to open the modal
-function openModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "flex"; // Display the modal with flex to center it
-    }
-    
-    // Function to close the modal
-    function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
-    }
-    
-    // Close the modal if the user clicks anywhere outside of the modal
-    window.onclick = function(event) {
-    var modal = document.getElementById("myModal");
-    if (event.target == modal) {
-    closeModal();
-    }
-    }*/
 // Function to open the modal with dynamic content
 function openModal(carTitle, carDescription) {
     var modal = document.getElementById("myModal");
